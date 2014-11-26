06ee97dfea1263588bb837298c52c16b9268b7a8
ysaito-m <y-saito@mediba.jp>
2014-11-26 19:45:28 +0900

    apacheインストール用レシピ修正

--- chef-repo/site-cookbooks/httpd/recipes/default.rb
+++ chef-repo/site-cookbooks/httpd/recipes/default.rb
@@ -14,3 +14,10 @@ service "httpd" do
   action [:enable,:start] #enable:os起動時に立ち上がる,start:走査時起動
   supports :status=>true,:restart=>true,:reload=>true #他のリソースなどに「status,restart,reloadが使えるよ」と教えるためのオプション。restartを書かないとstop+startで代用される。
 end
+
+template "/etc/httpd/conf/httpd.conf" do
+  owner "apache"
+  group "apache"
+  notifies :reload,'service[httpd]'
+end
+
